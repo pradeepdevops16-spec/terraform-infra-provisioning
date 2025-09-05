5ad64594494956a39f624ccefa51936997fdfcd3
pradeepdevops16-spec <pradeepdevops16@gmail.com>
2025-09-05 11:10:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,92 @@
-provider "aws" {
-  region = "us-west-1"
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "pradeep-terraform-bucket-2025-09-54"
-  # No ACL specified
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = "${var.region}a"
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_security_group" "instance" {
+  name        = "instance_sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.instance.name]
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-s3-bucket-${random_id.bucket_id.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_iam_role" "example" {
+  name = "example-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/terraform/log-group"
+  retention_in_days = 7
+}
